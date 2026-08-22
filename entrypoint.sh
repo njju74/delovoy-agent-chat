8bf637b40c0dee861cc1d52ce0ab237fec787b47
njju74 <njju74@ya.ru>
2026-08-22 23:46:46 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
 
+set -eu
+
+: "${CHAT_AUTH_USER:?Не задан CHAT_AUTH_USER}"
+: "${CHAT_AUTH_PASSWORD:?Не задан CHAT_AUTH_PASSWORD}"
+: "${CHAT_WEBHOOK_URL:?Не задан CHAT_WEBHOOK_URL}"
+
+htpasswd -bc /etc/nginx/.htpasswd \
+  "$CHAT_AUTH_USER" \
+  "$CHAT_AUTH_PASSWORD"
+
+envsubst '${CHAT_WEBHOOK_URL}' \
+  < /usr/share/nginx/html/config.template.js \
+  > /usr/share/nginx/html/config.js
+
+exec nginx -g 'daemon off;'
